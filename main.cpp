23009c2c1fa7a9ccf4a3c1c2b0cb13671849b1b2
sama-darya <sama.darya.uk@gmail.com>
2019-03-12 17:47:24 +0000

    prints the RGB values

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "clbp/Layer.h"
 #include "clbp/Net.h"
 
-#include "serial/serial.h"
-
 #include "opencv2/opencv.hpp"
 
+#include <iostream>
+
 using namespace cv;
+using namespace std;
+constexpr int ESC_key = 27;
 
 int main(int, char**)
 {
@@ -21,10 +23,25 @@ int main(int, char**)
         Mat frame;
         cap >> frame; // get a new frame from camera
         cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
+        //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        //Canny(edges, edges, 0, 30, 3);
         imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+        if(waitKey(1000) == ESC_key) break;
+        cout << frame.at<float>(20, 20) << endl;
+        auto intensity = frame.at<Vec3b>(100, 100);
+        cout << (int)intensity[0] << " " << (int)intensity[1] << " "  << (int)intensity[2] << endl;
+        //get differenceGreyValues as an array
+
+
+        //pass the pointer to the Net
+        //DoForward propagation
+        //getPredictiveAction
+        //pass the action to Robot
+        //get new image
+            //extract the leadError from it
+            //DoBackprop of the leadError
+            //extract new predictive differenceGreyValues
+
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
